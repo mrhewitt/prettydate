f81efb6acd4bbd9a4d734f7a1cec2eb96604a90d
unknown <mr.mark.hewitt@gmail.com>
2015-08-29 20:06:57 +0200

    Initial take-on

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# prettydate
-Angular service wrapping the Javascript Pretty Date
+Pretty Date
+===========
+
+Angular service built for use with requirejs and in particular SmartAdmin dashboard to convert ISO and Unix epoch dates into a human readable format.
+
+## Usage
+
+### Including the PrettyDate service into your SmartAdmin dashboard application, module and controller
+
+Create a prettydate folder in the js/plugin folder containing the .js file
+In your rconfig.js file add the line
+```javascript
+		'prettydate': '../plugin/prettydate/prettydate',
+```
+
+Make the service available to your module by including it in the module definition at the top of the file, for example
+
+```javascript
+define(['angular',
+    'angular-couch-potato',
+    'angular-ui-router',
+	'prettydate'
+], function (ng, couchPotato) {
+
+    "use strict";
+
+    var module = ng.module('app.mymodule', [ 'ui.router', 'PrettyDateService' ]);
+
+    couchPotato.configureApp(module)
+	
+	....
+	
+```
+
+Now in your controller your can inject the new service as you would in any regular Angular application, for example
+
+```javascript
+define(['../module'], function (module) {
+
+    'use strict';
+
+    return module.registerController('MyController', function ($scope, PrettyDate) {
+					
+	....
+	
+```
+
+Use the PrettyDate method as a regular function to format your date ISO strings or timestamps inside any of your methods in the injected module
+
+```javascript
+	var x = PrettyDate('2015-08-31 10:00');
+```
+
+## Meta
+
+Original script by the mighty [John Resig](https://github.com/jeresig).
+Modified by [Wylst](https://github.com/wylst).
+
+Licensed under the MIT and GPL licenses.
---
+++ src/prettydate.js
@@ -0,0 +1,78 @@
+/**
+ * prettydate
+ * https://github.com/mrhewitt/prettydate
+ * Copyright (c) 2015 Mark Hewitt (markhewitt.co.za)
+ *  Based on JavaScript Pretty Date
+ * Licensed MIT
+ *
+ * Angular prettyDate service
+ *
+ * Service wrapper prodiving an "angularized" access to the prettyDate jQuery formatter
+ * Takes a date either as a ISO string or a UNIX timestamps (seconds in epoch) and returns a
+ * descriptive string rather than an actual time, e.g. "Just now", "1 Hour ago", etc. If the 
+ * time argument is empty then it will return "Never"
+ *
+ * Designed for a SmartAdmin project, this is intended to be used with a requirejs enabled
+ * project, hence the define() wrapper
+ */
+
+define( ['angular'], function(angular) {
+	angular.module('PrettyDateService', []).factory('PrettyDate', function () {
+		   		   
+			/*
+			 * JavaScript Pretty Date
+			 * Modified 2015 by Mark Hewitt (markhewitt.co.za)
+			 * Modified 2013 by Alfred Xing (alfredxing.com)
+			 * Copyright (c) 2011 John Resig (ejohn.org)
+			 * Licensed under the MIT and GPL licenses.
+			 */
+			function prettyDate(time){
+				
+			  // if the date is empty then return the 	
+			  if ( !time ) { return	"Never"; }
+			  
+			  var date = new Date( typeof time === "String" ? (time || "").replace(/-/g,"/").replace(/[TZ]/g," ") : time*1000),
+				diff = (((new Date()).getTime() - date.getTime()) / 1000),
+				day_diff = Math.floor(diff / 86400),
+				out = "Some time ago";
+
+				switch(true) {
+					case diff < 60:
+					out = "Just now";
+					break;
+					case diff < 120:
+					out = "1 minute ago";
+					break;
+					case diff < 3600:
+					out = Math.floor( diff / 60 ) + " minutes ago";
+					break;
+					case diff < 7200:
+					out = "1 hour ago";
+					break;
+					case diff < 86400:
+					out = Math.floor( diff / 3600 ) + " hours ago";
+					break;
+					case day_diff == 1:
+					out = "Yesterday";
+					break;
+					case day_diff < 7:
+					out = day_diff + " days ago";
+					break;
+					case day_diff < 31:
+					out = Math.ceil( day_diff / 7 ) + " weeks ago";
+					break;
+					case day_diff < 366:
+					out = Math.floor( day_diff / 30 ) + " months ago";
+					break;
+					case day_diff > 365:
+					out = Math.floor( day_diff / 365 ) + " years ago";
+					break;
+				};
+
+				return out;
+			}
+
+			return prettyDate;
+		}
+	) 
+});
